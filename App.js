3302d2fa1839694833c806b2f0b0a198d90d5cdb
Fadi bakour <fadi.bakour101@hotmail.com>
2021-05-09 20:18:39 +0800

    change splashscreen and logo

--- App.js
+++ App.js
@@ -6,17 +6,20 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import Routes from './src/Routes/Routes'
 
 import { Provider } from 'react-redux';
 import { store, persistor } from './src/store/Store';
-import {PersistGate} from 'redux-persist/integration/react';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import Toast from 'react-native-toast-message';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
-
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
